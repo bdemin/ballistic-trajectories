a2ccc7c954c280966fbf3e0978e18a151bd87e77
bdemin <42113340+bdemin@users.noreply.github.com>
2019-08-08 16:55:03 +0300

    Fix negative y values

--- ballistics.py
+++ ballistics.py
@@ -9,15 +9,21 @@ theta = np.pi/4
 
 time_vector = np.linspace(0, 50, num = 1000)
 
-for v0 in v0_list: # Calculate trajectory for every initial speed
+for v0 in v0_list: # Calculate trajectory for every initial velocity
     x1 = []
     y1 = []
     for t in time_vector:
-        x = (v0*t) * np.cos(theta) # Get positions at every point in time
+        x = (v0*t) * np.cos(theta)
         y = (v0*t) * np.sin(theta) - (0.5*g) * (t**2)
         x1.append(x)
         y1.append(y)
         
-    plt.plot(x1, y1) # Plot for every angle
+    # Remove y values lower than 0
+    p = [i for i, j in enumerate(y1) if j < 0]                      
+    for i in sorted(p, reverse = True):
+        del x1[i]
+        del y1[i]
+
+    plt.plot(x1, y1)
 
 plt.show()
